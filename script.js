d9841ebc26464b5eb770da64c71e2956c8cb9875
sune3736 <79079294+sune3736@users.noreply.github.com>
2021-06-12 21:54:55 +0200

    Burger menu og header done

--- script.js
+++ script.js
@@ -15,6 +15,6 @@ function toggleMenu() {
   if (erSkjult == true) {
     document.querySelector("#menuknap").textContent = "☰";
   } else {
-    document.querySelector("#menuknap").textContent = "X";
+    document.querySelector("#menuknap").textContent = "✖";
   }
 }
