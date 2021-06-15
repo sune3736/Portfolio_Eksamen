c868bf4c193d9e4958d7a0fd2bb78d016a06e402
sune3736 <79079294+sune3736@users.noreply.github.com>
2021-06-15 15:06:27 +0200

    web section og skellet done

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ window.addEventListener("load", sidenVises);
 function sidenVises() {
   console.log("sidenVises");
   document.querySelector("#menuknap").addEventListener("click", toggleMenu);
+
   document.querySelector(".se_mere").addEventListener("click", toggleMere);
 }
 
